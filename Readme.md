30c559e1cf2f0105f4dffee3ffe6f00b6926660e
simontamene.dev@gmail.com <simontamene.dev@gmail.com>
2024-08-02 09:33:02 +0300

    lesson4

--- Readme.md
+++ Readme.md
@@ -1,6 +1,4 @@
 <h1>PHP Full Course</h1>
-<p>PHP Strings</p>
-<p>PHP - Modify Strings</p>
-<p>PHP - Slicing Strings</p>
-<p>PHP - Escape Characters</p>
-<p>PHP Numbers</p>
+<p>PHP Casting</p>
+<p>PHP Math</p>
+<p>PHP Constants</p>
--- index.php
+++ index.php
@@ -11,71 +11,171 @@
 </html>
 <?php
 
-// String Length
-echo strlen("Hello world!");
-// Word Count
-echo str_word_count("Hello world!");
+// Change Data Type
+// (string) - Converts to data type String
+// (int) - Converts to data type Integer
+// (float) - Converts to data type Float
+// (bool) - Converts to data type Boolean
+// (array) - Converts to data type Array
+// (object) - Converts to data type Object
+// (unset) - Converts to data type NULL
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean
+$e = NULL;    // NULL
 
-// Search For Text Within a String
+$a = (string) $a;
+$b = (string) $b;
+$c = (string) $c;
+$d = (string) $d;
+$e = (string) $e;
 
-echo strpos("Hello world!", "world");
-
-// Upper Case
-$x = "Hello World!";
-echo strtoupper($x);
-// Lower Case
-$x = "Hello World!";
-echo strtolower($x);
-// Replace String
-$x = "Hello World!";
-echo str_replace("World", "Dolly", $x);
-// Reverse a String
-$x = "Hello World!";
-echo strrev($x);
-// Remove Whitespace
-$x = " Hello World! ";
-echo trim($x);
-// Convert String into Array
-$x = "Hello World!";
-$y = explode(" ", $x);
-
-echo "<br><br><br>";
-//Use the print_r() function to display the result:
-print_r($y);
-
-echo "<br><br><br>";
-// Start the slice at index 6 and end the slice 5 positions later:
-$x = "Hello World!";
-echo substr($x, 6, 5);
-// By leaving out the length parameter, 
-// the range will go to the end:
-$x = "Hello World!";
-echo substr($x, 6);
-// To insert characters that are illegal in a string,
-//  use an escape character.
-$x = "We are the so-called \"Vikings\" from the north.";
-
-$a = 5;
-$b = 5.34;
-$c = "25";
-
+//To verify the type of any object in PHP, use the var_dump() function:
 var_dump($a);
 var_dump($b);
 var_dump($c);
+var_dump($d);
+var_dump($e);
 
-// Check if the type of a variable is integer:
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "25 kilometers"; // String
+$d = "kilometers 25"; // String
+$e = "hello"; // String
+$f = true;    // Boolean
+$g = NULL;    // NULL
 
-$x = 5985;
-var_dump(is_int($x));
+$a = (int) $a;
+$b = (int) $b;
+$c = (int) $c;
+$d = (int) $d;
+$e = (int) $e;
+$f = (int) $f;
+$g = (int) $g;
 
-$x = 59.85;
-var_dump(is_int($x));
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "25 kilometers"; // String
+$d = "kilometers 25"; // String
+$e = "hello"; // String
+$f = true;    // Boolean
+$g = NULL;    // NULL
 
-// Check if the type of a variable is float:
-    $x = 10.365;
-var_dump(is_float($x));
+$a = (float) $a;
+$b = (float) $b;
+$c = (float) $c;
+$d = (float) $d;
+$e = (float) $e;
+$f = (float) $f;
+$g = (float) $g;
 
-// Invalid calculation will return a NaN value:
-$x = acos(8);
-var_dump($x);
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = 0;       // Integer
+$d = -1;      // Integer
+$e = 0.1;     // Float
+$f = "hello"; // String
+$g = "";      // String
+$h = true;    // Boolean
+$i = NULL;    // NULL
+
+$a = (bool) $a;
+$b = (bool) $b;
+$c = (bool) $c;
+$d = (bool) $d;
+$e = (bool) $e;
+$f = (bool) $f;
+$g = (bool) $g;
+$h = (bool) $h;
+$i = (bool) $i;
+
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean
+$e = NULL;    // NULL
+
+$a = (array) $a;
+$b = (array) $b;
+$c = (array) $c;
+$d = (array) $d;
+$e = (array) $e;
+
+echo(pi());
+
+echo(min(0, 150, 30, 20, -8, -200));
+echo(max(0, 150, 30, 20, -8, -200));
+
+echo(abs(-6.7));
+
+echo(sqrt(64));
+
+echo(round(0.60));
+echo(round(0.49));
+
+echo(rand());
+
+echo(rand(10, 100));
+
+// PHP Constants
+// A constant is an identifier (name) for a simple value. The value cannot be changed during the script.
+
+// A valid constant name starts with a letter or underscore (no $ sign before the constant name).
+
+// Note: Unlike variables, constants are automatically global across the entire script.
+
+function n()
+{
+    define('A',8);
+}
+n();
+echo A;
+const MYCAR = "Volvo";
+echo MYCAR;
+
+define("GREETING", "Welcome to W3Schools.com!");
+
+function myTest() {
+  echo GREETING;
+}
+
+myTest();
+
+// const vs. define()
+
+// const are always case-sensitive
+// define() has a case-insensitive option.
+// const cannot be created inside another block scope, like inside a function or inside an if statement.
+// define can be created inside another block scope.
+
+// PHP has nine predefined constants that change value depending on where they are used, and therefor they are called "magic constants".
+
+// __CLASS__	If used inside a class, the class name is returned.	
+// __DIR__	The directory of the file.	
+// __FILE__	The file name including the full path.	
+// __FUNCTION__	If inside a function, the function name is returned.	
+// __LINE__	The current line number.	
+// __METHOD__	If used inside a function that belongs to a class, both class and function name is returned.	
+// __NAMESPACE__	If used inside a namespace, the name of the namespace is returned.	
+// __TRAIT__	If used inside a trait, the trait name is returned.	
+// ClassName::class	Returns the name of the specified class and the name of the namespace, if any.
+echo " ".__DIR__;
+
+// Short Hand If
+
+$a = 13;
+
+$b = $a < 10 ? "Hello" : "Good Bye";
+
+echo $b;
+
+$a = 13;
+
+$b = $a < 10 ? "Hello" : "Good Bye";
+
+echo $b;
+echo "<br><br><br><br><br><br><br>";
+print_r($_SERVER['REQUEST_METHOD']);
 ?>

117e3b70edfd91ea570205d997e8cffebbb40f28
simontamene.dev@gmail.com <simontamene.dev@gmail.com>
2024-08-02 06:46:42 +0300

    lesson3

--- Readme.md
+++ Readme.md
@@ -1,3 +1,6 @@
 <h1>PHP Full Course</h1>
-<p>PHP echo and print Statements</p>
-<p>PHP Data Types</p>
+<p>PHP Strings</p>
+<p>PHP - Modify Strings</p>
+<p>PHP - Slicing Strings</p>
+<p>PHP - Escape Characters</p>
+<p>PHP Numbers</p>
--- index.php
+++ index.php
@@ -11,95 +11,71 @@
 </html>
 <?php
 
-// echo and print are more or less the same. They are both used to output data to the screen.
-// The differences are small: 
-// echo has no return value while print has a return value of 1 so it can be used in expressions. echo can take multiple parameters (although such usage is rare) while print can take one argument. echo is marginally faster than print.
-echo "Hello";
-//same as:
-echo("Hello");
+// String Length
+echo strlen("Hello world!");
+// Word Count
+echo str_word_count("Hello world!");
 
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
+// Search For Text Within a String
 
-$txt1 = "Learn PHP";
-$txt2 = "W3Schools.com";
+echo strpos("Hello world!", "world");
 
-echo "<h2>$txt1</h2>";
-echo "<p>Study PHP at $txt2</p>";
+// Upper Case
+$x = "Hello World!";
+echo strtoupper($x);
+// Lower Case
+$x = "Hello World!";
+echo strtolower($x);
+// Replace String
+$x = "Hello World!";
+echo str_replace("World", "Dolly", $x);
+// Reverse a String
+$x = "Hello World!";
+echo strrev($x);
+// Remove Whitespace
+$x = " Hello World! ";
+echo trim($x);
+// Convert String into Array
+$x = "Hello World!";
+$y = explode(" ", $x);
 
-$txt1 = "Learn PHP";
-$txt2 = "W3Schools.com";
-
-echo '<h2>' . $txt1 . '</h2>';
-echo '<p>Study PHP at ' . $txt2 . '</p>';
-
-print "Hello";
-//same as:
-print("Hello");
-
-print "<h2>PHP is Fun!</h2>";
-print "Hello world!<br>";
-print "I'm about to learn PHP!";
-
-
-$txt1 = "Learn PHP";
-$txt2 = "W3Schools.com";
-
-print "<h2>" . $txt1 . "</h2>";
-print "<p>Study PHP at " . $txt2 . "</p>";
-
-// PHP supports the following data types:
-
-// String
-// Integer
-// Float (floating point numbers - also called double)
-// Boolean
-// Array
-// Object
-// NULL
-// Resource
-
-//integer
-$x = 5;
-var_dump($x);
-
-//String
-$x = "Hello world!";
-$y = 'Hello world!';
-
-var_dump($x);
-echo "<br>";
-var_dump($y);
-
-//Float
-$x = 10.365;
-var_dump($x);
-
-//Array
-
-$cars = array("Volvo","BMW","Toyota");
-var_dump($cars);
 echo "<br><br><br>";
-class Vichel
-{
-    public $wheel;
-    public $size;
-    function __construct($wheel,$size)
-    {
-        $this->wheel = $wheel;
-        $this->size = $size;
-    }
-    function hi()
-    {
-        echo "$this->wheel";
-    }
-}
+//Use the print_r() function to display the result:
+print_r($y);
 
-$car = new Vichel(12,44);
-$car->hi();
-var_dump($car);
-// PHP NULL Value
-$x = "Hello world!";
-$x = null;
+echo "<br><br><br>";
+// Start the slice at index 6 and end the slice 5 positions later:
+$x = "Hello World!";
+echo substr($x, 6, 5);
+// By leaving out the length parameter, 
+// the range will go to the end:
+$x = "Hello World!";
+echo substr($x, 6);
+// To insert characters that are illegal in a string,
+//  use an escape character.
+$x = "We are the so-called \"Vikings\" from the north.";
+
+$a = 5;
+$b = 5.34;
+$c = "25";
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+
+// Check if the type of a variable is integer:
+
+$x = 5985;
+var_dump(is_int($x));
+
+$x = 59.85;
+var_dump(is_int($x));
+
+// Check if the type of a variable is float:
+    $x = 10.365;
+var_dump(is_float($x));
+
+// Invalid calculation will return a NaN value:
+$x = acos(8);
 var_dump($x);
 ?>
